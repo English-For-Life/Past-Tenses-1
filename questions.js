f978b5c6aadd4161ce449561ffa4ee8db911e87b
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 20:42:20 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I told John not to drive, because he _____ all night.",
+        options:["had been drinking", "had drunk", "was drinking", "drank"],
+        answer: 0
+    },
+    {
+     q:"Why did you change the channel? I ______ that movie.",
+        options:["watched", "was watching", "had watched", "had been watching"],
+        answer: 1   
+    },
+    {
+        q:"When I _____ out of the shower the lights _____ .",
+        options:["got / were getting off", "had got / were getting off", "was getting / went off", "had been getting / had got off"],
+        answer: 2
+    },
+    {
+        q:"When I saw Clara I realised that she ______ much in all those years.",
+        options:["didn't change", "wasn't changing", "hadn't been changing", "hadn't changed"],
+        answer: 3
+    },
+    {
+        q:"When he entered the room somebody _____ him in the head.",
+        options:["hit", "was hitting", "had hit", "had been hitting"],
+        answer: 0
+    },
+    {
+        q:"He admitted that he ______ all the money from the box.",
+        options:["took", "had taken", "was taking", "had been taking"],
+        answer: 1
+    },
+    {
+        q:"The burglar _____ to open the safe when he _____ the owner come in.",
+        options:["tried / was hearing", "had been trying / had heard", "was trying / heard", " tried / heard"],
+        answer: 2
+    },
+    {
+        q:"I knew he _____ because his eyes were red.",
+        options:["cried", "was crying", "had cried", " had been crying"],
+        answer: 3
+    },
+    {
+        q:"The plane in which the football team _____ crashed and none of them survived.",
+        options:["were travelling", "travelled", "had travelled", "had been travelling"],
+        answer: 0
+    },
+    {
+        q:"We _____ for about 20 minutes when the plane was hit by turbulence.",
+        options:["had flown", "had been flying", "were flying", "flew"],
+        answer: 1
+    },
+    {
+        q:"Many years ago I _____ to spend New Year's Eve with my family.",
+        options:["was deciding", "decided", "had decided", "had been deciding"],
+        answer: 1
+    },
+    {
+     q:"Unfortunately, I _____ nothing new or exciting to tell them, just the same sad old stories.",
+        options:["had", "was", "had had", "was having"],
+        answer: 0   
+    },
+    {
+        q:"My sisters _____ at me for the whole evening.",
+        options:["loked", "were looking", "had been looking", "had looked"],
+        answer: 2
+    },
+    {
+        q:"When we finished dinner I _____ my glass.",
+        options:["was raising", "had been raising", "had raised", "raised"],
+        answer: 3
+    },
+    {
+        q:"By the time we _____ to the theatre, the play _____ .",
+        options:["had been got / was starting", "got / started", "got / had already started", "had got / already started"],
+        answer: 2
+    }
+]
